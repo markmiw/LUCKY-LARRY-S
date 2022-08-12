ed8aeb9e4f42c3f4d7e1c62eb06a7e1c5c6f398d
Cornelius Renken <cornelius.renken@gmail.com>
2022-08-12 10:50:39 -0700

    added router and basic file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import {
+  BrowserRouter, Routes, Route, Navigate,
+} from 'react-router-dom';
+import Homepage from './homepage/Homepage';
+import Roulette from './roulette/Roulette';
+import ScratchTicket from './scratch-ticket/ScratchTicket';
+import Slots from './slots/Slots';
 
-function App() {
+function Router() {
   return (
-    <div>
-      Hello World!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/roulette"
+          element={<Roulette />}
+        />
+        <Route
+          path="/scratch-ticket"
+          element={<ScratchTicket />}
+        />
+        <Route
+          path="/slots"
+          element={<Slots />}
+        />
+        <Route
+          path="/"
+          element={<Homepage />}
+        />
+        <Route
+          path="*"
+          element={<Navigate to="/" />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default Router;
