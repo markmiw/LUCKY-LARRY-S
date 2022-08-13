6d19da34961420aa246418318d43ae1465a98207
Cornelius Renken <cornelius.renken@gmail.com>
2022-08-12 17:08:00 -0700

    basic slot machine carousel

---
+++ client/src/components/slots/Column.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+
+export default function Column({ scrollTime, values, setValues }) {
+  const [offset, setOffset] = useState(0);
+
+  const columnRef = useRef(null);
+
+  useEffect(() => {
+    const transitionEndListener = (e) => {
+      e.stopPropagation();
+      setValues(values.slice(-3));
+      columnRef.current.style.transition = '';
+      setOffset(0);
+    };
+
+    columnRef.current.addEventListener('transitionend', transitionEndListener);
+    return () => { columnRef.current.addEventListener('transitionend', transitionEndListener); };
+  }, [columnRef, values]);
+
+  useEffect(() => {
+    if (values.length !== 3) {
+      columnRef.current.style.transition = `bottom ${scrollTime}s ease-out`;
+      setOffset(30 * (values.length - 3));
+    }
+  }, [columnRef, values]);
+
+  return (
+    <div
+      style={{
+        display: 'inline-block',
+        backgroundColor: 'rgb(220, 220, 220)',
+        height: '90px',
+        width: '40px',
+        marginRight: '10px',
+        overflow: 'hidden',
+      }}
+    >
+      <div
+        ref={columnRef}
+        style={{
+          position: 'relative',
+          bottom: `${offset}px`,
+          transition: `bottom ${scrollTime}s ease-out`,
+        }}
+      >
+        {
+          values.map((value) => (
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '30px',
+                width: '40px',
+              }}
+            >
+              {value}
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
+}
+
+Column.propTypes = {
+  scrollTime: PropTypes.number.isRequired,
+  values: PropTypes.arrayOf(PropTypes.number).isRequired,
+  setValues: PropTypes.func.isRequired,
+};
--- client/src/components/slots/Slots.jsx
+++ client/src/components/slots/Slots.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Column from './Column';
 
 export default function Slots() {
+  const [column1Values, setColumn1Values] = useState([1, 2, 3]);
+  const [column2Values, setColumn2Values] = useState([4, 5, 6]);
+  const [column3Values, setColumn3Values] = useState([7, 8, 9]);
+
+  function getSlotArray(start, result) {
+    const filler = [...new Array(50)].map(() => Math.floor(Math.random() * 5 + 1));
+    return start.concat(filler, result);
+  }
+
+  function play() {
+    // don't allow playing when a roll is in progress
+    if (column3Values.length !== 3) {
+      return;
+    }
+    // get values from andy
+    const result = [7, 7, 7, 6, 6, 6, 5, 5, 5];
+    setColumn1Values(getSlotArray(column1Values, result.slice(0, 3)));
+    setColumn2Values(getSlotArray(column2Values, result.slice(3, 6)));
+    setColumn3Values(getSlotArray(column3Values, result.slice(6)));
+  }
+
   return (
-    <div>
-      Slots!
-    </div>
+    <>
+      <Column
+        scrollTime={4}
+        values={column1Values}
+        setValues={setColumn1Values}
+      />
+      <Column
+        scrollTime={5}
+        values={column2Values}
+        setValues={setColumn2Values}
+      />
+      <Column
+        scrollTime={6}
+        values={column3Values}
+        setValues={setColumn3Values}
+      />
+      <button
+        type="button"
+        style={{
+          marginTop: '50px',
+        }}
+        onClick={() => {
+          play();
+        }}
+      >
+        Go!
+      </button>
+    </>
   );
 }
