4ba5e3c1728aa0125227ccbe6ca6e92233ecb045
Cornelius Renken <cornelius.renken@gmail.com>
2022-08-13 17:58:36 -0700

    implemented function to calculate slot winnings and return visual info to the client

--- server/controllers/slots.js
+++ server/controllers/slots.js
@@ -1,3 +1,5 @@
+const { getBalance, updateBalanceBasedOnWinnings } = require('../../database/controllers');
+
 module.exports.spin = (req, res) => {
   const combination = [];
   const { plays } = req.query;
@@ -7,12 +9,11 @@ module.exports.spin = (req, res) => {
   res.status(201).send(combination);
 };
 
-module.exports.weightedSpin = (req, res) => {
+const weightedSpin = (plays) => {
   const combination = [];
-  const { plays } = req.query;
   for (let i = 0; i < (3 * plays); i += 1) {
     if (i % 3 === 1) {
-      const chance = Math.random() * 100 + 1;
+      const chance = Math.random() * 100;
       const choices = [1, 2, 3, 4, 5];
       choices.splice(combination[i - 1] - 1, 1);
       if (chance <= 50) {
@@ -27,7 +28,7 @@ module.exports.weightedSpin = (req, res) => {
         combination.push(choices[3]);
       }
     } else if ((i % 3 === 2) && (combination[i - 2] === combination[i - 1])) {
-      const chance = Math.random() * 100 + 1;
+      const chance = Math.random() * 100;
       const choices = [1, 2, 3, 4, 5];
       choices.splice(combination[i - 1] - 1, 1);
       if (chance <= 10) {
@@ -45,5 +46,51 @@ module.exports.weightedSpin = (req, res) => {
       combination.push(Math.floor(Math.random() * 5) + 1);
     }
   }
-  res.status(201).send(combination);
+  return combination;
+};
+
+const calculateWinnings = (slotMatrix, bet, rows) => {
+  const multipliers = {
+    1: 1,
+    2: 3,
+    3: 5,
+    4: 10,
+    5: 50,
+  };
+
+  const winnings = {
+    currency: 0,
+    winningRows: [],
+  };
+
+  for (let i = 0; i < rows; i += 1) {
+    const row = slotMatrix.slice(3 * i, 3 * i + 3);
+    if (row.every((item) => item === row[0])) {
+      winnings.currency += bet * multipliers[row[0]];
+      winnings.winningRows.push(i + 1);
+    }
+  }
+
+  return winnings;
+};
+
+module.exports.slots = async (req, res) => {
+  const { userid, bet, rows } = req.body;
+  if (!userid || !bet || !rows) {
+    res.status(400).send('missing input');
+    return;
+  }
+  const userHasEnoughMoney = await getBalance(userid) >= bet;
+  if (!userHasEnoughMoney) {
+    res.status(200).send('insufficient funds');
+    return;
+  }
+  const slotResult = weightedSpin(3);
+  const winnings = calculateWinnings(slotResult, bet, rows);
+  const updatedBalance = await updateBalanceBasedOnWinnings(userid, bet, winnings.currency);
+  res.status(201).json({
+    rows: slotResult,
+    updatedBalance,
+    winnings,
+  });
 };
--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
-const slots = require('./controllers/slots');
+const { slots } = require('./controllers/slots');
 const { getUser } = require('../database/controllers');
 
-// router.get('/slots', slots.spin);
-// router.get('/weightedSlots', slots.weightedSpin);
+router.put('/slots', slots);
 
 router.get('/test', (req, res) => {
   res.status(200).send('hello!');
