5af699226c60c3cf7e8d8fe750b464218d3e65c7
markmiw <markmiw6@gmail.com>
2022-08-16 09:26:44 -1000

    updated router

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -2,10 +2,6 @@ import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter, Routes, Route, Navigate,
 } from 'react-router-dom';
-// import Homepage from './components/homepage/Homepage';
-// import Roulette from './components/roulette/Roulette';
-// import ScratchTicket from './components/scratch-ticket/ScratchTicket';
-// import Slots from './components/slots/Slots';
 import Sidebar from './components/sidebar/Sidebar';
 import Homepage from './components/homepage/Homepage';
 import Roulette from './components/roulette/Roulette';
@@ -31,14 +27,14 @@ function Router() {
 
   return (
     <BrowserRouter>
-      {/* <NavBar
+      <NavBar
         user={user}
         setUser={setUser}
         loggedIn={loggedIn}
         setLoggedIn={setLoggedIn}
-      /> */}
+      />
       <Routes>
-        {/* <Route
+        <Route
           path="/roulette"
           element={<Roulette />}
         />
@@ -49,15 +45,19 @@ function Router() {
         <Route
           path="/slots"
           element={<Slots />}
-        /> */}
+        />
         <Route
           path="/sidebar"
           element={<Sidebar />}
         />
-        {/* <Route
+        <Route
           path="/"
           element={<Homepage />}
-        /> */}
+        />
+        <Route
+          path="*"
+          element={<Navigate to="/" />}
+        />
       </Routes>
     </BrowserRouter>
   );
