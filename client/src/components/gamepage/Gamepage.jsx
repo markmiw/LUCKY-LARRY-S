2af689c0ab4a1632a4c056db029ea01e6a3cb468
markmiw <markmiw6@gmail.com>
2022-08-16 14:31:49 -1000

    removed DS_Store

--- client/src/components/gamepage/Gamepage.jsx
+++ client/src/components/gamepage/Gamepage.jsx
@@ -5,17 +5,17 @@ import Roulette from '../roulette/Roulette';
 import ScratchTicket from '../scratch-ticket/ScratchTicket';
 import Slots from '../slots/Slots';
 
-function Gamepage({ user, game }) {
-  const renderGame = () => {
-    if (game === 'roulette') {
-      return <Roulette />;
-    }
-    if (game === 'scratch-ticket') {
-      return <ScratchTicket />;
-    }
-    return <Slots />;
-  };
+const renderGame = () => {
+  if (game === 'roulette') {
+    return <Roulette />;
+  }
+  if (game === 'scratch-ticket') {
+    return <ScratchTicket />;
+  }
+  return <Slots />;
+};
 
+function Gamepage({ game }) {
   return (
     <div className="game-container">
       <Sidebar user={user} />
